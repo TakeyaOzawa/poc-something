c4f4227f4425691c9e836a652f0e1a194b86b6d9
TakeyaOzawa <takeya_ozawa@nyle.co.jp>
2025-10-29 01:17:28 +0900

    バージョン管理漏れファイルの追加

---
+++ q/scripts/install-aws-tools.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p /opt/aws-cli
+
+HOST_ARCH=$(uname -m)
+if [ "$HOST_ARCH" = "arm64" ] || [ "$HOST_ARCH" = "aarch64" ]; then
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2-arm.zip" && \
+    unzip awscliv2-arm.zip && \
+    ./aws/install --install-dir /opt/aws-cli/aarch64 --bin-dir /opt/aws-cli/aarch64/bin && \
+    rm -rf awscliv2-arm.zip aws && \
+    ln -sf /opt/aws-cli/aarch64/bin/aws /usr/local/bin/aws && \
+    echo "Using ARM64 AWS CLI"
+else
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2-x86.zip" && \
+    unzip awscliv2-x86.zip && \
+    ./aws/install --install-dir /opt/aws-cli/x86_64 --bin-dir /opt/aws-cli/x86_64/bin && \
+    rm -rf awscliv2-x86.zip aws && \
+    ln -sf /opt/aws-cli/x86_64/bin/aws /usr/local/bin/aws && \
+    echo "Using x86_64 AWS CLI"
+fi
